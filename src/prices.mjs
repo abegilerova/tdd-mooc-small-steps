a75d1303c56a6ca4b825d78f75b5a59ffa59754a
Aikanysh Begilerova <abegilerova@gmail.com>
2022-10-30 23:11:07 -0500

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -89,6 +89,7 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
+      console.log(row.holiday)
       let holiday2 = parseTemporal(row.holiday);
       if (
         date &&
