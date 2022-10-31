b1fe316b01940b010daf3473220c8cab39ac8551
Aikanysh Begilerova <abegilerova@gmail.com>
2022-10-30 23:15:23 -0500

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -17,7 +17,6 @@ function createApp(database) {
     const age = req.query.age;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parseDate(req.query.date);
     const temporal = parseTemporal(req.query.date);
     const cost = calculateCost(age, type, baseCost, temporal);
     res.json({ cost });
